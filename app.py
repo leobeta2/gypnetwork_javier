8b0b531f9d48d0ad73d31246b2e1ccf7cc07e95f
leobeta2 <leobeta2@gmail.com>
2015-03-19 21:04:46 -0300

    se definieron las paginas y se conectaron entre ellas

--- app.py
+++ app.py
@@ -5,13 +5,26 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/inicio')
 def home():
     return render_template('index.html')
 
-@app.route('/contact')
+@app.route('/sobre')
+def about():
+    return render_template('about.html')
+
+@app.route('/ubicacion')
+def ubica():
+    return render_template('ubicacion.html')
+
+@app.route('/galeria')
+def galeria():
+    return render_template('portfolio-3-col.html')
+
+
+@app.route('/contacto')
 def contact():
     return render_template('contact.html')
-
 
 
 if __name__ == '__main__':
